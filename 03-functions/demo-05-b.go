a7eab160227ed3c421ea6698414834601a5a4acf
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-08 02:53:02 +0530

    assignment - solution (function composition)

--- 03-functions/demo-05-b.go
+++ 03-functions/demo-05-b.go
@@ -11,11 +11,15 @@ func main() {
 	add(100, 200)
 	subtract(100, 200)
 
-	log(100, 200, add)
-	log(100, 200, subtract)
+	/* log(100, 200, add)
+	log(100, 200, subtract) */
 
 	/* create 2 functions which when invoked will perform add / subtract with logging */
+	logAdd := log(add)
+	logAdd(100, 200)
 
+	logSubtract := log(subtract)
+	logSubtract(100, 200)
 }
 
 func add(x, y int) {
@@ -26,8 +30,16 @@ func subtract(x, y int) {
 	fmt.Println("Subtract result =", x-y)
 }
 
-func log(x, y int, operation func(int, int)) {
+/* func log(x, y int, operation func(int, int)) {
 	fmt.Println("invocation started")
 	operation(x, y)
 	fmt.Println("invocation completed")
+} */
+
+func log(operation func(int, int)) func(int, int) {
+	return func(x, y int) {
+		fmt.Println("invocation started")
+		operation(x, y)
+		fmt.Println("invocation completed")
+	}
 }
