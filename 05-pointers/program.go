3dc5e87feb023ab9b348770858952e0713e733b9
Magesh Kuppan <tkmagesh77@gmail.com>
2022-03-09 00:31:19 +0530

    pointers example

--- 05-pointers/program.go
+++ 05-pointers/program.go
@@ -16,8 +16,20 @@ func main() {
 	fmt.Println("Before incrementing, no = ", no)
 	increment(&no)
 	fmt.Println("After incrementing, no = ", no)
+
+	n1, n2 := 10, 20
+	fmt.Printf("Before swaping, n1 = %d & n2 = %d\n", n1, n2)
+	swap(&n1, &n2)
+	fmt.Printf("Before swaping, n1 = %d & n2 = %d\n", n1, n2)
 }
 
 func increment(no *int) {
 	*no++
+}
+
+func swap(x, y *int) /* No return values */ {
+	var temp = *x
+	*x = *y
+	*y = temp
+	//*x, *y = *y, *x
 }
